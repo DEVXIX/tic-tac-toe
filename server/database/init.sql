7d5d3d7b051ceefc80934f764ae033a4798388a1
DEVXIX <divine1v9@gmail.com>
2025-11-19 00:14:33 +0400

    fix db setup initilization

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,2 +1,19 @@
 -- Database initialization
--- Prisma will handle table creation via migrations
+-- Create role if it doesn't exist
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'beaconred') THEN
+
+      CREATE ROLE "beaconred" LOGIN PASSWORD 'beaconred';
+   END IF;
+END
+$do$;
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON DATABASE db TO "beaconred";
+
+-- Allow user to create databases (needed for Prisma)
+ALTER USER "beaconred" CREATEDB;
